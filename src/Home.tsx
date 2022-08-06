3eff261bede02f50d76eca494549054a593fd963
Nie-Mand <chill.out.x19@gmail.com>
2022-08-06 23:00:23 +0100

    test trapezoid thing

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,21 @@
 import { useAccelerometer } from './Accelerometer'
 
+function getClipPath(alpha: number) {
+  return `polygon(0 0, 100% 0, ${100 - alpha}% 100%, ${alpha}% 100%)`
+}
+
 export default function Home() {
   const { x, y, z, rotation } = useAccelerometer()
   return (
-    <div>
-      <h1>x: {x}</h1>
-      <h1>y: {y}</h1>
-      <h1>z: {z}</h1>
-      <h1>
-        rotation: ({rotation?.alpha}, {rotation?.beta}, {rotation?.gamma}){' '}
-      </h1>
+    <div className="w-full h-screen grid place-content-center">
+      <img
+        src="https://img.seadn.io/files/7c09bb8adc54f991b4f12f45e7906f61.png?auto=format&fit=max&w=512"
+        alt="nft"
+        className="duration-200"
+        style={{
+          clipPath: getClipPath(rotation!.alpha || 0),
+        }}
+      />
     </div>
   )
 }
